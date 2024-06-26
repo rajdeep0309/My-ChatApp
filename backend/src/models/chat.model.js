d5ac9cb18727a80cf635b354c43a38d149655d27
abhik <abhik@gmail.com>
2024-06-26 09:23:44 +0530

    Changes to Models In Backend

--- backend/src/models/chat.model.js
+++ backend/src/models/chat.model.js
@@ -1,28 +1,50 @@
-import mongoose from "mongoose"
-const chatSchema=new mongoose.Schema(
-    {
-        sender:{
-            type:mongoose.Schema.Types.ObjectId,
-            ref:'User',
-            required:true,
-        },
-        receiver:{
-            type:mongoose.Schema.Types.ObjectId,
-            ref:'User',
-            required:true,
-        },
-        message:{
-            type:String,
-            required:true,
-        },
-        isRead:{
-            type:Boolean,
-            default:false,
-        },
+import mongoose from "mongoose";
+// const chatSchema=new mongoose.Schema(
+//     {
+//         sender:{
+//             type:mongoose.Schema.Types.ObjectId,
+//             ref:'User',
+//             required:true,
+//         },
+//         receiver:{
+//             type:mongoose.Schema.Types.ObjectId,
+//             ref:'User',
+//             required:true,
+//         },
+//         message:{
+//             type:String,
+//             required:true,
+//         },
+//         isRead:{
+//             type:Boolean,
+//             default:false,
+//         },
+//     },
+//     {
+//         timestamps:true,
+//     }
+// );
+// const Chat=mongoose.model('Chat',chatSchema);
+const chatModel = mongoose.Schema(
+  {
+    chatName: { type: String },
+    isGroupChat: { type: Boolean },
+    users: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+    latestMessage: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Message",
     },
-    {
-        timestamps:true,
-    }
+    groupAdmin: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+  },
+  { timeStamp: true }
 );
-const Chat=mongoose.model('Chat',chatSchema);
+const Chat = mongoose.model("Chat", chatModel);
 export default Chat;
