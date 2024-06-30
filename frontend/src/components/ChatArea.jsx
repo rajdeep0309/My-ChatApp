675ba3b2d2580daa47470c93e52e137cdcdcbad5
Rajdeep <rajdeep3.9ghosh@gmail.com>
2024-06-30 21:43:18 +0530

    Revert "1st Iteration Complete"
    This reverts commit 3d28ec90815d0915cbb5a1392533c889f9ed206b.

--- frontend/src/components/ChatArea.jsx
+++ frontend/src/components/ChatArea.jsx
@@ -9,33 +9,29 @@ import { ContactContext } from '../store/contact-details-context';
 import Profile from './list/right_sidebar/Profile';
 import { motion, useScroll } from 'framer-motion';
 import { useParams } from 'react-router-dom';
-import { Socket, io } from 'socket.io-client';
-import axios, { all } from 'axios';
+import axios from 'axios';
 
-const ENDPOINT = 'http://localhost:3001';
 const nameFormat = (e) => {
 	return e
 		.split(' ')
 		.map((word) => word.charAt(0).toUpperCase() + word.slice(1).toLowerCase())
 		.join(' ');
 };
-var socket, chat;
+
 function ChatArea({ activeUser }) {
 	console.log('This is from chatarea');
 	console.log(activeUser);
 	const message = useRef();
 	const [finalMessage, setFinalMessage] = useState('');
 	const [allMessages, setAllMessages] = useState([]);
-	const [allMessagesCopy, setAllMessagesCopy] = useState([]);
 	const conCtx = useContext(ContactContext);
 	console.log(conCtx._id);
 	const [selectedContact, setSelectedContact] = useState(null);
-	const [socketConnectionStatus, setSocketConnectionStatus] = useState(false);
 	const token = localStorage.getItem('accessToken');
 	console.log(token);
-	const dataid = JSON.parse(localStorage.getItem('userData'));
-	console.log('kloppppppp');
-	console.log(dataid.data.data.user._id);
+	const dataid = JSON.parse(localStorage.getItem("userData"))
+	console.log("kloppppppp")
+	console.log(dataid.data.data.user._id)
 	// const {accessToken, refreshToken, user} = dataid.data.data.user;
 	// const userId = user._id;
 	const sendMessage = () => {
@@ -60,22 +56,7 @@ function ChatArea({ activeUser }) {
 				console.log(data);
 			});
 	};
-	useEffect(() => {
-		socket = io(ENDPOINT);
-		socket.emit('setup', dataid.data.data.user);
-		socket.on('connection', () => {
-			setSocketConnectionStatus(!socketConnectionStatus);
-		});
-	}, []);
 
-	useEffect(() => {
-		socket.on('message recieved', (newMessage) => {
-			if (!allMessagesCopy || allMessagesCopy._id !== newMessage._id) {
-			} else {
-				setAllMessages([...allMessages], newMessage);
-			}
-		});
-	});
 	useEffect(() => {
 		console.log('Users refreshed');
 		const config = {
@@ -87,12 +68,10 @@ function ChatArea({ activeUser }) {
 			.get('http://localhost:3001/api/v1/message/' + activeUser, config)
 			.then(({ data }) => {
 				setAllMessages(data);
-				socket.emit('join_chat', activeUser);
 				// console.log("Data from Acess Chat API ", data);
 			});
-		setAllMessagesCopy(allMessages);
 		// scrollToBottom();
-	}, [activeUser, token, allMessages]);
+	}, [activeUser, token]);
 	console.log(allMessages);
 	function handleSubmit() {
 		setFinalMessage(message.current.value);
@@ -106,22 +85,9 @@ function ChatArea({ activeUser }) {
 	const closeProfile = () => {
 		setSelectedContact(null);
 	};
-
-	const chatContainerRef = useRef(null);
-	useEffect(() => {
-		if (chatContainerRef.current) {
-			chatContainerRef.current.scrollTop =
-				chatContainerRef.current.scrollHeight;
-		}
-	}, [allMessages]);
 	return (
 		<>
-			<div
-				className='chat-area'
-				ref={chatContainerRef}
-				style={{
-					overflowY: 'scroll',
-				}}>
+			<div className='chat-area'>
 				<div className='chat-area-header'>
 					<div
 						className='chat-area-header-text'
@@ -141,18 +107,21 @@ function ChatArea({ activeUser }) {
 				</div>
 
 				<div className='chat-area-messages'>
-					{allMessages.slice(0).map((message, index) => {
-						const sender = message.sender;
-						const self_id = dataid.data.data.user._id;
-
-						if (sender._id === self_id) {
-							console.log('I sent it ');
-							return <MessageSelf props={message} key={index} />;
-						} else {
-							console.log('Someone Sent it');
-							return <MessageOthers props={message} key={index} />;
-						}
-					})}
+					{allMessages
+						.slice(0)
+						.reverse()
+						.map((message, index) => {
+							const sender = message.sender;
+							const self_id = dataid.data.data.user._id;
+							
+							if (sender._id === self_id) {
+								console.log('I sent it ');
+								return <MessageSelf props={message} key={index} />;
+							} else {
+								console.log('Someone Sent it');
+								return <MessageOthers props={message} key={index} />;
+							}
+						})}
 				</div>
 				<div className='text-input-area'>
 					<input
